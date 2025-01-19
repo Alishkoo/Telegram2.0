3a88e5d1f90f5b131b2d40617e990e8d90908de5
Alishkoo <baisholanov.alibek@gmail.com>
2025-01-19 22:40:58 +0500

    lets go

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Alibek Baisholanov on 19.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
