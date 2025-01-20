afa625920c2c1db35f008073e0f6aaa4d4154265
Alishkoo <baisholanov.alibek@gmail.com>
2025-01-20 23:08:19 +0500

    login vc

---
+++ Messenger/Controllers/Login/LoginViewController.swift
@@ -0,0 +1,128 @@
+//
+//  LoginViewController.swift
+//  Messenger
+//
+//  Created by Alibek Baisholanov on 19.01.2025.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    //MARK: UIViews
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.clipsToBounds = true
+        return scrollView
+    }()
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "logo3")
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 12
+        imageView.layer.masksToBounds = true
+        return imageView
+    }()
+    
+    private let emailField: UITextField = {
+        let field = UITextField()
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.returnKeyType = .continue // Sets the return key to say “Continue” instead of “Return.”
+        field.layer.cornerRadius = 12
+        field.layer.borderWidth = 1 // Adds a 1-point border around the text field.
+        field.layer.borderColor = UIColor.lightGray.cgColor
+        field.placeholder = "Email Address..."
+        
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 0))
+        field.leftViewMode = .always
+        field.backgroundColor = .white
+        
+        return field
+    }()
+    
+    private let passwordField: UITextField = {
+        let field = UITextField()
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.returnKeyType = .done // sets the return to say "Return"
+        field.layer.cornerRadius = 12
+        field.layer.borderWidth = 1 // Adds a 1-point border around the text field.
+        field.layer.borderColor = UIColor.lightGray.cgColor
+        field.placeholder = "Password..."
+        field.isSecureTextEntry = true
+        
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 0))
+        field.leftViewMode = .always
+        field.backgroundColor = .white
+        
+        return field
+    }()
+    
+    private let loginButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Log In", for: .normal)
+        button.backgroundColor = .link
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 12
+        button.layer.masksToBounds = true
+        button.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
+        return button
+    }()
+    
+    //MARK: ViewDidLoad()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "Log in"
+        view.backgroundColor = .white
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Register",
+                                                            style: .done,
+                                                            target: self,
+                                                            action: #selector(didTapRegister))
+        
+        //MARK: Add subviews
+        view.addSubview(scrollView)
+        scrollView.addSubview(imageView)
+        scrollView.addSubview(emailField)
+        scrollView.addSubview(passwordField)
+        scrollView.addSubview(loginButton)
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        //MARK: Set frames to scrollViews
+        scrollView.frame = view.bounds
+        
+        let size = view.width / 3
+        imageView.frame = CGRect(x: (view.width - size) / 2,
+                                 y: 20,
+                                 width: size,
+                                 height: size)
+        emailField.frame = CGRect(x: 30,
+                                  y: imageView.bottom + 10,
+                                  width: scrollView.width - 60,
+                                  height: 52)
+        passwordField.frame = CGRect(x: 30,
+                                  y: emailField.bottom + 10,
+                                  width: scrollView.width - 60,
+                                  height: 52)
+        loginButton.frame = CGRect(x: 30,
+                                  y: passwordField.bottom + 10,
+                                  width: scrollView.width - 60,
+                                  height: 52)
+    }
+    
+    //MARK: func register
+    @objc private func didTapRegister(){
+        let vc = RegisterViewController()
+        vc.title = "Create Account"
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+
+}
